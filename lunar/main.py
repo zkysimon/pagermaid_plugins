66ca445a237eea81d66b486fe4363d388250a773
zkysimon <zkygo@outlook.com>
2022-07-04 13:59:40 +0800

    Update main.py

--- lunar/main.py
+++ lunar/main.py
@@ -16,4 +16,10 @@ async def lunar(msg: Message):
         lunar = Lunar.fromDate(datetime.now())
     except:
         return await msg.edit("可能未安装模块，请使用`pip3 install lunar_python`安装")
-    await msg.edit(f"{solar.toFullString()}\r\n\r\n{lunar.toFullString()}")
+    yiji = "宜："
+    for i in lunar.getDayYi():
+    	yiji += f"{i} "
+    yiji += "\r\n忌："
+    for i in lunar.getDayJi():
+    	yiji += f"{i} "
+    await msg.edit(f"{solar.toFullString()}\r\n\r\n{lunar.toFullString()}\r\n{yiji}")
