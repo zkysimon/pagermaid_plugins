53b2590e5717e2c37cd8a47c6663131a88989aaa
zkysimon <zkygo@outlook.com>
2022-07-04 12:06:08 +0800

    Create main.py

---
+++ lunar/main.py
@@ -0,0 +1,19 @@
+from datetime import datetime
+from lunar_python import Solar, Lunar
+from pagermaid.listener import listener
+from pagermaid.utils import Message
+
+
+@listener(
+    is_plugin=True,
+    outgoing=True,
+    command="lunar",
+    description="查询今日黄历"
+)
+async def lunar(msg: Message):
+    try:
+        solar = Solar.fromDate(datetime.now())
+        lunar = Lunar.fromDate(datetime.now())
+    except:
+        return await msg.edit("可能未安装模块，请使用`pip3 install lunar_python`安装")
+    await msg.edit(f"{solar.toFullString()}\r\n\r\n{lunar.toFullString()}")
